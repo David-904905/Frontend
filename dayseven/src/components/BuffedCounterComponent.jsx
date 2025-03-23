ed45717897429a9de952634ec8f6044a4c426b4e
David <davidnduonofit47@gmail.com>
2025-03-23 15:16:32 +0100

    mend
    day seven continued

--- dayseven/src/components/BuffedCounterComponent.jsx
+++ dayseven/src/components/BuffedCounterComponent.jsx
@@ -1,4 +1,4 @@
-import { useReducer } from "react";
+import { useReducer, useState } from "react";
 
 import reducer from "../utilities/BuffedCounterUtil";
 
@@ -9,23 +9,42 @@ const initialState = {
 
 const BuffedCounterComponent = () => {
     const [state, dispatch] = useReducer(reducer, initialState);
-    
+    const [specialValue, setSpecialValue] = useState("");
+    const [operation, setOperation] = useState("increment");
+
+
+    const handleInputChange = (e) => {
+        setSpecialValue(e.target.value);
+    }
+
     const handleSpecialRequest = (e) => {
         e.preventDefault();
-        dispatch({type: "user-increment", inputValue: e.target.value})
-        
-    
+        if(!isNaN(specialValue) && specialValue.trim() !== ""){
+            dispatch({type: operation == "increment" ? "user-increment" : "user-decrement", inputValue: Number(specialValue)})
+        }  
+    };
+
+    const handleOperationChange = (e) => {
+        setOperation(e.target.value);
     }
+
     return (
         <>
             <p>Count: {state.count}</p>
             <button onClick={() => dispatch({type: "increment"})}>+</button>
             <button onClick={() => dispatch({type: "decrement"})}>-</button>
             <button onClick={() => dispatch({type: "reset"})}>reset</button>
-            <form onChange={handleSpecialRequest}>
-                <label htmlFor="special-increment">Special Increment</label>
-                <input type="text" id="special-increment" name="special-increment" />
-                <button type="submit">S+</button>
+            <form onSubmit={handleSpecialRequest}>
+                <label htmlFor="special-operation">Special Increment</label>
+                <input type="text" id="special-operation" name="special-operation" value={specialValue} onChange={handleInputChange}/>
+                
+                <select value={operation} onChange={handleOperationChange} name="operation-select" id="operation-select">
+                    <option value="increment">increment</option>
+                    <option value="decrement">decrement</option>
+                </select>
+
+
+                <button type="submit">Perform Operation</button><br />
             </form>
         </>
     )
